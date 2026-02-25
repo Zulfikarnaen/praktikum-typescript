5b8d27d7dac87f105c7621188992cb23dd06ce35
Zulfikarnaen <h1101241009@student.untan.ac.id>
2026-02-25 04:43:07 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return `Selamat Datang ` + nama + ` di Kelas PWL `;
+}
+const mahasiswa = 'Andi';
+console.log(hello(mahasiswa));
+//# sourceMappingURL=index.js.map
