b5db139f389836da1028791b62256bc7e177da52
Zulfikarnaen <h1101241009@student.untan.ac.id>
2026-02-25 10:23:47 +0000

    Program Sederhana dengan Tipe Data

---
+++ src/tugas1.ts
@@ -0,0 +1,7 @@
+function sapa(nama: string, umur: number): string {
+    return `Halo ${nama} tahun ini umurmu ${umur+1} tahun`;
+}
+
+const nama = 'Zulfi';
+const umur = 19;
+console.log(sapa(nama, umur));
