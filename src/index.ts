bad6cc80a03ab69130dbd141a40d3e2b5a8d7935
Zulfikarnaen <h1101241009@student.untan.ac.id>
2026-02-25 04:46:26 +0000

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function hello(nama: string, umur: number): string {
     return `Selamat Datang `+ nama +` di Kelas PWL, umur `+ nama +` adalah `+ umur +``;
 }
 
-const mahasiswa ='Andi';
+const mahasiswa ='Zulfi';
 const umur = 19;
 console.log(hello(mahasiswa, umur));
